1ac24b0922bdc77fb98a1ee021f6ca0bdd04c3e3
safwan <safwan.mah99@gmail.com>
2020-08-09 23:55:50 -0700

    slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+ 
+  cart = [];
+  items = [];
+ 
+  sliderConfig = {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: 1.6,
+   
+    
+  };
+ 
+  constructor(private cartService:CartService, private router: Router ) { }
+ 
+  ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+ 
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+ 
+  openCart() {
+    this.router.navigate(['cart']);
+  }
 }
